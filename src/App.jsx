f1f91119cc8ee82d1128b9ba865d1e15c61b7057
Ashwani kumar <dearashi45@gmail.com>
2026-03-29 02:48:01 +0530

    Gallery project done alomst

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,63 @@
 import axios from 'axios'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const App = () => {
-  // const user = {
-  //   user: "Ashi",
-  //   age: 18,
-  //   city: "siwan",
-  // }
-  //  localStorage.setItem("info",JSON.stringify(user));
-  //  const l=JSON.parse(localStorage.getItem("info"));
-
-  //  console.log(l.user)
-  // const getData = async () => {
-  //   const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
-  //   // const l = await response.json();
-  //   console.log(response.data);
-  // }
 
   const [user, setUser] = useState([]);
+  const [index, setIndex] = useState(1)
+
   const getData = async () => {
-    const response = await axios.get('https://picsum.photos/v2/list');
-    console.log(response.user);
-    setUser(response.user);
+    const response = await axios.get(`https://picsum.photos/v2/list?page=${index}&limit=10`);
+    // console.log(response.data);
+    setUser(response.data);
   }
 
-  const response = "Loading...";
-  
-  return (
-    <div>
-      <button
-        onClick={getData}
-        className='bg-red-950 rounded p-4 m-4 text-xl text-white active:scale-95 cursor-pointer'> Click me !</button>
-      <div>
-        {user.map(function (elem, idx) {
-          return <div>
-            <h3>hello {idx}</h3>
+  useEffect(() => {
+    getData();
+  }, [index]);
+
+  let printUserData = <h3 className='text-gray-400 text-xl absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+    Loading... </h3>
+
+  if (user.length > 0) {
+    printUserData = user.map(function (elem, idx) {
+      return <div key={idx}>
+        <a href={elem.url} target='_blank'>
+          <div className='bg-white h-70 w-62 overflow-hidden rounded-xl'>
+            <img className='h-full w-full object-cover ' src={elem.download_url} alt="photo" />
           </div>
-        })}
+          <h2 className='font-semibold text-lg'>{elem.author}</h2>
+        </a>
       </div>
-    </div>
+    })
+  }
+
+  return (
+    <div className='h-screen overflow-auto text-white bg-black '>
+
+      <div className='flex flex-wrap gap-3 p-4 h-full'>
+        {printUserData}
+      </div>
+
+      <div className='flex justify-center item-center text-white p-5 gap-4 font-semibold text-lg'>
+
+        <button
+          style={{ opacity: index == 1 ? 0.5 : 1 }}
+          onClick={function () {
+            if (idx > 0) {
+              setIndex(index - 1);
+              setUser([]);
+            }
+          }}
+          className=' w-20 h-10  bg-amber-500  active:scale-95   rounded cursor-pointer' > Prev </button >
+        <h2> Page no-{index}</h2>
+
+        <button onClick={function () {
+          setUser([]);
+          setIndex(index + 1);
+        }}
+          className=' w-20 h-10  bg-amber-500  active:scale-95 rounded cursor-pointer' > Next </button ></div>
+    </div >
   )
 }
 
